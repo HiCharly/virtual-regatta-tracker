fa86b4582a5851898888c76c58f00a3a2e66ea2c
HiCharly <caillon.charly@gmail.com>
2025-01-30 22:55:30 +0100

    Feat : full screen

--- main.js
+++ main.js
@@ -193,4 +193,16 @@ resetButton.addEventListener('click', function () {
     resetAnimation();
 });
 
+fullScreen.addEventListener('click', function () {
+    const elem = document.documentElement
+
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+});
+
 startAnimation()
